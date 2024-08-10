9c8d1b72cb5988ecb671fd50295f60b1dfda414d
Himanshu <himanubhai1234@gmail.com>
2024-08-10 18:12:59 +0530

    variables done

--- 01-Basics/01_vairables.js
+++
@@ -1,3 +0,0 @@
-const himanshu= 122334
-let himanshu1 = himanshu
-console.log(himanshu1) // Output: 122334
---
+++ 01-Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 122334
+let accountEmail = "himanshu@gmail.com"
+var password = "khkhsfd"
+accountCity= 'Gurgaon'
+let accountState // we can declare it anytime we want
+//accountId= 1234      //not allowed because const cant be changed
+accountEmail= "himanssu@gmail.com"
+password= "wfkkiwu"
+accountCity= "Jalandhar"
+console.log(accountId) // Output: 122334
+console.log(accountEmail) // Output: himanssu@gmail.com
+console.log(password) // Output: wfkkiwu
+console.log(accountCity) // Output: Jalandhar
+console.log(accountState) // Output: Undefined
+console.table([accountId, accountEmail, password, accountCity, accountState]) //Gives the outputs in table with indexes just like array starting from 0
