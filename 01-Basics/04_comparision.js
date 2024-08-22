07c2608b4ec4ed5790915a7a8d6c440267c47404
Himanshu <himanubhai1234@gmail.com>
2024-08-22 21:40:29 +0530

    ..

---
+++ 01-Basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//* niche diye gye dastawezon ka prayog na hi kre to accha h ye bs seekhne k liye tha baki isme bhut dikkt aa skti h 
+
+console.log(null > 0);// false
+console.log(null == 0);// false
+console.log(null >= 0);// true (discripency bhi bol skte h but ye null ko 0 m convert kr deta h)
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+// ===   strictly check the value and datatype both
+
+console.log("2" === 2);//false
