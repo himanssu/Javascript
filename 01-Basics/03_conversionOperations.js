339eec6b70ee6f9bc683044bae38338562cec712
Himanshu <himanubhai1234@gmail.com>
2024-08-22 20:23:03 +0530

    ..

--- 01-Basics/03_conversionOperations.js
+++ 01-Basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "himanshu"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "himanshu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
@@ -26,3 +26,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
  //console.log(stringNumber);  output:- 33
 // console.log(typeof stringNumber); output:- string
+
+
+// *************************Operations************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);// output:- -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // power
+// console.log(2/3);
+// console.log(2%3); //remainder
+
+let str1 = "hello"
+let str2 = " himanshu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //output:- 122 (kyuki jo chiz bracket m pehle ayegi usi k hisab se proyority milegi k kaise treat krna h)
+// console.log(1 + 2 + "2"); // output:- 32 (kyuki isme pehle no. tha phir string to pehle no. add hogye)
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);// 1(conversion)
+// console.log(+"");//0
+
+let x = 100
+++x;
+console.log(x);
