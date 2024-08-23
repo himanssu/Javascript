9c6abfbd32d7afd8608401413c1403dc107b98b5
Himanshu <himanubhai1234@gmail.com>
2024-08-23 12:49:00 +0530

    datatypes-summary

---
+++ 01-Basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// there are two types of datatypes
+// 1. primitive datatypes(call by value)
+// 2. non-primitive datatypes(call by reference)
+
+// javascript is a static language but can be dynamic by the use of web api's
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction); // return function but in documentaion its stated as function object.
