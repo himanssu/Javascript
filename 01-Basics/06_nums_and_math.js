003581b489b992de71f8d042a2ca061c817a1b50
Himanshu <himanubhai1234@gmail.com>
2024-08-25 22:41:33 +0530

    math

--- 01-Basics/06_nums_and_math.js
+++ 01-Basics/06_nums_and_math.js
@@ -14,3 +14,22 @@ const otherNumber = 123.8966
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString()) // converts the no. into string and adds commas for better in the american standards
 // console.log(hundreds.toLocaleString('en-IN'));// converts into indian standards
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));// will give positive values only 
+// console.log(Math.round(4.6));// round off value
+// console.log(Math.ceil(4.2));// will give 5 means top value
+// console.log(Math.floor(4.9));// will give  lower integer
+// console.log(Math.min(4, 3, 6, 8));// minimun value
+// console.log(Math.max(4, 3, 6, 8));// max value
+
+console.log(Math.random());// will give randon value between 0 and 1 will also include 0 and one
+console.log((Math.random()*10) + 1);// we will prefer this in various scenerios because if we got the value like 0.03 then after multiplying it will still return 0.3 so to avoid this we add 1
+console.log(Math.floor(Math.random()*10) + 1);//will give floor value and give the value between 1 and 9
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)// will give random values between the given numbers
