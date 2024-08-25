0c1cacd934766ab5b4a1691f2c551d756029b42e
Himanshu <himanubhai1234@gmail.com>
2024-08-25 21:32:00 +0530

    nums

---
+++ 01-Basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400 // it is automatically defined that this is no.
+// console.log(score);
+
+const balance = new Number(100)// this is another way of defining no. for confirm defining of something
+// console.log(balance);
+
+// console.log(balance.toString().length);// convert the no. into string and then give the length of it
+// console.log(balance.toFixed(1));// this converts the no. to a string and rounds the string to a specific no. of decimals
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4)); precise value upto the given point
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString()) // converts the no. into string and adds commas for better in the american standards
+// console.log(hundreds.toLocaleString('en-IN'));// converts into indian standards
